a625ba9b828b023c56aa52479ffba84e6378a895
侯雨 <houyu@le.com>
2023-10-19 10:43:16 +0800

    feat: update

--- src/views/course-management/Event/index.vue
+++ src/views/course-management/Event/index.vue
@@ -1,11 +1,87 @@
 <script setup lang="ts">
+import { ref } from "vue";
 defineOptions({
   name: ""
 });
+
+const formInline = ref({
+  user: "",
+  region: "",
+  date: ""
+});
+
+const onSubmit = () => {
+  console.log("submit!");
+};
+
+const tableData = [
+  {
+    date: "2016-05-03",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles"
+  },
+  {
+    date: "2016-05-02",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles"
+  },
+  {
+    date: "2016-05-04",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles"
+  },
+  {
+    date: "2016-05-01",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles"
+  }
+];
 </script>
 
 <template>
   <div>
-    <el-card>22</el-card>
+    <el-card>
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="Approved by">
+          <el-input
+            v-model="formInline.user"
+            placeholder="Approved by"
+            clearable
+          />
+        </el-form-item>
+        <el-form-item label="Activity zone">
+          <el-select
+            v-model="formInline.region"
+            placeholder="Activity zone"
+            clearable
+          >
+            <el-option label="Zone one" value="shanghai" />
+            <el-option label="Zone two" value="beijing" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="Activity time">
+          <el-date-picker
+            v-model="formInline.date"
+            type="date"
+            placeholder="Pick a date"
+            clearable
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">Query</el-button>
+        </el-form-item>
+      </el-form>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="Date" width="180" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address" />
+      </el-table>
+    </el-card>
   </div>
 </template>
+
+<style>
+.demo-form-inline .el-input {
+  --el-input-width: 220px;
+}
+</style>
